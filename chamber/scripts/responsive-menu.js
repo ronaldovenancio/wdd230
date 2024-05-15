b044dfcb6734389d73296cbe583be0c42a93a97d
Ronaldo Venâncio da Silva <95707584+ronaldovenanciorvs@users.noreply.github.com>
2024-05-15 11:50:39 -0300

    upload files

--- chamber/scripts/responsive-menu.js
+++ chamber/scripts/responsive-menu.js
@@ -1,12 +1,11 @@
 /* Responsive Menu Code */
 
-const menuButton = document.querySelector("#menu-button");
-const menuIcon = document.querySelector("#menu-icon");
-const menuItems = document.querySelectorAll(".menu-item"); // menuitems will be a Node List of the list items
+const menuButton = document.querySelector("#nav-button");
+const menuIcon = document.querySelector("#nav-icon");
+const menuItems = document.querySelectorAll(".nav-li");
 
 menuButton.addEventListener("click", () => {
 	menuItems.forEach((item) => item.classList.toggle("open"));
-	menuButton.classList.toggle("closed");
 	menuButton.classList.toggle("active");
 	menuIcon.classList.toggle("fa-bars");
 	menuIcon.classList.toggle("fa-x");
