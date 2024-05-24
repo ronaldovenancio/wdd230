44d4674fa613c7a55aad9de2e63f03f188e0715f
Ronaldo Venâncio da Silva <95707584+ronaldovenanciorvs@users.noreply.github.com>
2024-05-24 10:08:16 -0300

    upload files

--- scripts/links.js
+++ scripts/links.js
@@ -1,52 +1,51 @@
 const baseURL = "https://ronaldovenancio.github.io/wdd230/";
 const linksURL = "https://ronaldovenancio.github.io/wdd230/data/links.json";
 
-  
-  async function getLinks() {
-      try {
-          const response = await fetch(linksURL);
-          if (!response.ok) {
-              throw new Error("Failed to fetch data");
-          }
-          const data = await response.json();
-          // console.log(data);
-          displayLinks(data.lessons);
-          return data;
-      } catch (error) {
-          console.error("Error fetching data:", error);
-          throw error;
-      }
-  }
-  getLinks();
-  
-  const displayLinks = (weeks) => {
-      // console.log(weeks);
-  
-      weeks.forEach((week) => {
-          // console.log(week);
-          const listItem = document.createElement("li");
-          const lessonWeek = document.createTextNode(`Lesson ${week.lesson}: `);
-  
-          listItem.appendChild(lessonWeek);
-  
-          week.links.forEach((link) => {
-              // console.log(link);
-              const tag = document.createElement("a");
-              tag.setAttribute("href", link.url);
-              tag.textContent = link.title;
-  
-              listItem.appendChild(tag);
-  
-              if (week.links.length > 1 && week.links.indexOf(link) < week.links.length - 1) {
-                  listItem.innerHTML += " | ";
-              }
-          });
-          // console.log(listItem);
-  
-          const list = document.querySelector(".la-card");
-          list.appendChild(listItem);
-      });
-  };
+async function getLinks() {
+    try {
+        const response = await fetch(linksURL);
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        // console.log(data);
+        displayLinks(data.lessons);
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+}
+getLinks();
+
+const displayLinks = (weeks) => {
+    // console.log(weeks);
+
+    weeks.forEach((week) => {
+        // console.log(week);
+        const listItem = document.createElement("li");
+        const lessonWeek = document.createTextNode(`Lesson ${week.lesson}: `);
+
+        listItem.appendChild(lessonWeek);
+
+        week.links.forEach((link) => {
+            // console.log(link);
+            const tag = document.createElement("a");
+            tag.setAttribute("href", link.url);
+            tag.textContent = link.title;
+
+            listItem.appendChild(tag);
+
+            if (week.links.length > 1 && week.links.indexOf(link) < week.links.length - 1) {
+                listItem.innerHTML += " | ";
+            }
+        });
+        // console.log(listItem);
+
+        const list = document.querySelector(".la-card");
+        list.appendChild(listItem);
+    });
+};
   /*
 const requestURL = 'data/data.json'; // retrieve the local JSON file
 const output = document.querySelector("#json-ouput")
